46a66513981e867b5eb711f413a53b3f0dd6a3b2
singcheong <95837196+singcheong@users.noreply.github.com>
2023-12-15 14:18:04 +0800

    Create diy-part1.sh

---
+++ config/openwrt-22.03/diy-part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/openwrt/openwrt / Branch: main
+#========================================================================================================================
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# other
+# rm -rf package/utils/{ucode,fbtest}
+rm -rf openwrt-custom openwrt-custom
+git clone https://github.com/kenzok8/small-package.git openwrt-custom
+
+sed -i '$a src-link custom openwrt-custom' feeds.conf.default
